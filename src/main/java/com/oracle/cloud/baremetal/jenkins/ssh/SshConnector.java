2b57ee6fd2dca41806df102ef8a007cd094a63d6
Filip Krestan <filip.krestan@oracle.com>
2018-02-06 10:32:00 +0100

    Do not use http proxy for ssh connection to agents

--- src/main/java/com/oracle/cloud/baremetal/jenkins/ssh/SshConnector.java
+++ src/main/java/com/oracle/cloud/baremetal/jenkins/ssh/SshConnector.java
@@ -1,15 +1,8 @@
 package com.oracle.cloud.baremetal.jenkins.ssh;
 
 import java.io.IOException;
-import java.net.InetSocketAddress;
-import java.net.Proxy;
-
-import com.oracle.cloud.baremetal.jenkins.JenkinsUtil;
 import com.trilead.ssh2.Connection;
-import com.trilead.ssh2.HTTPProxyData;
 import com.trilead.ssh2.ServerHostKeyVerifier;
-
-import hudson.ProxyConfiguration;
 
 public class SshConnector {
 
@@ -17,25 +10,8 @@ public class SshConnector {
 
     private static final int PORT = 22;
 
-    public ProxyConfiguration getProxyConfiguration() {
-        return JenkinsUtil.getJenkinsInstance().proxy;
-    }
-
     public Connection createConnection(String host) throws IOException, InterruptedException {
-        Connection conn = new Connection(host, PORT);
-        ProxyConfiguration proxyConfig = getProxyConfiguration();
-        Proxy proxy = proxyConfig == null ? Proxy.NO_PROXY : proxyConfig.createProxy(host);
-        if (!proxy.equals(Proxy.NO_PROXY) && proxy.address() instanceof InetSocketAddress) {
-            InetSocketAddress address = (InetSocketAddress) proxy.address();
-            HTTPProxyData proxyData = null;
-            if (null != proxyConfig.getUserName()) {
-                proxyData = new HTTPProxyData(address.getHostName(), address.getPort(), proxyConfig.getUserName(), proxyConfig.getPassword());
-            } else {
-                proxyData = new HTTPProxyData(address.getHostName(), address.getPort());
-            }
-            conn.setProxyData(proxyData);
-        }
-        return conn;
+        return new Connection(host, PORT);
     }
 
     public void connect(Connection conn, int timeoutMillis) throws IOException {
