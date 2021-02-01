f468f1925d4aa5cfee8431558f6892932c33426a
Andrey Andreyev <andrey.adnreyev@oracle.com>
2021-02-01 10:25:50 +0300

    Remove unused import

--- src/main/java/com/oracle/cloud/baremetal/jenkins/client/BaremetalCloudClientFactory.java
+++ src/main/java/com/oracle/cloud/baremetal/jenkins/client/BaremetalCloudClientFactory.java
@@ -1,7 +1,5 @@
 package com.oracle.cloud.baremetal.jenkins.client;
 
-import com.oracle.cloud.baremetal.jenkins.credentials.BaremetalCloudCredentials;
-
 public interface BaremetalCloudClientFactory {
     BaremetalCloudClient createClient(String credentialsId, int maxAsyncThreads);
 }
