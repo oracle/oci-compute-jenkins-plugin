35880090dafab4fa25b0d38b268d07185db2f756
sindyarl <sindhu.sri.y.yarlagadda@oracle.com>
2022-05-12 14:53:40 +0530

    'Delete Agent'-redirect to home page

--- src/main/java/com/oracle/cloud/baremetal/jenkins/BaremetalCloudComputer.java
+++ src/main/java/com/oracle/cloud/baremetal/jenkins/BaremetalCloudComputer.java
@@ -1,9 +1,28 @@
 package com.oracle.cloud.baremetal.jenkins;
 
 import hudson.slaves.AbstractCloudComputer;
+import org.kohsuke.stapler.HttpRedirect;
+import org.kohsuke.stapler.HttpResponse;
+import org.kohsuke.stapler.HttpResponses;
+
+import java.io.IOException;
 
 public class BaremetalCloudComputer extends AbstractCloudComputer<BaremetalCloudAgent> {
     public BaremetalCloudComputer(BaremetalCloudAgent slave) {
         super(slave);
+    }
+
+    @Override
+    public HttpResponse doDoDelete() throws IOException {
+        checkPermission(DELETE);
+        try {
+            BaremetalCloudAgent node = getNode();
+            if (node != null) { // No need to terminate nodes again
+                node.terminate();
+            }
+            return new HttpRedirect("../..");
+        } catch (InterruptedException e) {
+            return HttpResponses.error(500, e);
+        }
     }
 }
